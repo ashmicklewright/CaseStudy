1df76dd78a85ab99c7f0f66cd84563c998fc6c42
Ashley Micklewright <ashmicklewright@gmail.com>
2026-05-05 21:13:42 -0700

    Committing app to GitHub.

--- CaseStudyApp/app.R
+++ CaseStudyApp/app.R
@@ -1,44 +1,149 @@
-## case study app
+# case study ER app
+# chapter 4 in Hadley's textbook
 
 library(shiny)
+library(vroom)
+library(tidyverse)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+dir.create("neiss", showWarnings = FALSE)
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+download <- function(name) {
+  url <- "https://raw.github.com/hadley/mastering-shiny/main/neiss/"
+  download.file(paste0(url, name), paste0("neiss/", name), quiet = TRUE)
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+download("injuries.tsv.gz")
+download("population.tsv")
+download("products.tsv")
+
+injuries <- vroom::vroom("neiss/injuries.tsv.gz")
+products <- vroom::vroom("neiss/products.tsv")
+population <- vroom::vroom("neiss/population.tsv")
+
+prod_codes <- setNames(products$prod_code, products$title)
+
+count_top <- function(df, var, n = 5) {
+  df %>%
+    mutate({{ var }} := fct_lump(fct_infreq({{ var }}), n = n)) %>%
+    group_by({{ var }}) %>%
+    summarise(n = as.integer(sum(weight)))
+}
+
+ui <- fluidPage(
+  titlePanel("ER Injury Explorer"),
+  
+  fluidRow(
+    column(
+      8,
+      selectInput(
+        "code",
+        "Product",
+        choices = prod_codes,
+        width = "100%"
+      )
+    ),
+    column(
+      4,
+      selectInput(
+        "y",
+        "Y axis",
+        choices = c("rate", "count")
+      )
+    )
+  ),
+  
+  fluidRow(
+    column(4, h4("Diagnosis"), tableOutput("diag")),
+    column(4, h4("Body Part"), tableOutput("body_part")),
+    column(4, h4("Location"), tableOutput("location"))
+  ),
+  
+  fluidRow(
+    column(12, plotOutput("age_sex"))
+  ),
+  
+  fluidRow(
+    column(2, actionButton("story", "Tell me a story")),
+    column(10, textOutput("narrative"))
+  )
+)
+
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+
+
+
+server <- function(input, output, session) {
+  
+  selected <- reactive({
+    injuries %>%
+      filter(prod_code == input$code)
+  })
+  
+  output$diag <- renderTable(
+    count_top(selected(), diag),
+    width = "100%"
+  )
+  
+  output$body_part <- renderTable(
+    count_top(selected(), body_part),
+    width = "100%"
+  )
+  
+  output$location <- renderTable(
+    count_top(selected(), location),
+    width = "100%"
+  )
+  
+  summary <- reactive({
+    selected() %>%
+      count(age, sex, wt = weight) %>%
+      left_join(population, by = c("age", "sex")) %>%
+      mutate(rate = n / population * 1e4)
+  })
+  
+  output$age_sex <- renderPlot({
+    if (input$y == "count") {
+      summary() %>%
+        ggplot(aes(age, n, colour = sex)) +
+        geom_line() +
+        labs(
+          x = "Age",
+          y = "Estimated number of injuries",
+          colour = "Sex"
+        )
+    } else {
+      summary() %>%
+        ggplot(aes(age, rate, colour = sex)) +
+        geom_line(na.rm = TRUE) +
+        labs(
+          x = "Age",
+          y = "Injuries per 10,000 people",
+          colour = "Sex"
+        )
+    }
+  }, res = 96)
+  
+  narrative_sample <- eventReactive(
+    list(input$story, selected()),
+    selected() %>%
+      pull(narrative) %>%
+      sample(1)
+  )
+  
+  output$narrative <- renderText({
+    narrative_sample()
+  })
+}
+
+
+### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+ 
+shinyApp(ui, server)
+
+
+
+
+
+
+
